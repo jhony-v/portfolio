919a9eacef2b9d8b2f11ec4c218c176b0f59f7fe
jhony <jhonyvegacuya24@gmail.com>
2020-05-29 19:33:21 -0500

    create structure scroll horizontal component

---
+++ src/app/shared/containers/scroll-horizontal-behavior/scroll-horizontal-behavior.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-horizontal-behavior',
+  templateUrl: './scroll-horizontal-behavior.component.html',
+  styleUrls: ['./scroll-horizontal-behavior.component.styl']
+})
+export class ScrollHorizontalBehaviorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
