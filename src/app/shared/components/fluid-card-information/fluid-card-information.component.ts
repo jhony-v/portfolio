6c102d9242d56e3e565a636733aa6110920c654f
jhony <jhonyvegacuya24@gmail.com>
2020-06-02 18:53:00 -0500

    add input to fluid-card-information and create shadow style

--- src/app/shared/components/fluid-card-information/fluid-card-information.component.ts
+++ src/app/shared/components/fluid-card-information/fluid-card-information.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fluid-card-information',
   templateUrl: './fluid-card-information.component.html',
-  styleUrls: ['./fluid-card-information.component.styl']
+  styleUrls: ['./fluid-card-information.component.styl'],
 })
 export class FluidCardInformationComponent implements OnInit {
+  @Input('title') title: string;
+  @Input('detail') detail: string;
+  @Input('image-avatar') imageAvatar: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
