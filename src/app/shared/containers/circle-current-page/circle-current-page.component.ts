17d438fe70b88b7bd0b750374163875811477d0a
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-07-03 16:17:30 -0500

    add selectable circle current page

--- src/app/shared/containers/circle-current-page/circle-current-page.component.ts
+++ src/app/shared/containers/circle-current-page/circle-current-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-circle-current-page',
   templateUrl: './circle-current-page.component.html',
-  styleUrls: ['./circle-current-page.component.styl']
+  styleUrls: ['./circle-current-page.component.styl'],
 })
 export class CircleCurrentPageComponent implements OnInit {
+  allLinksRouters: string[] = [
+    '/home',
+    '/history',
+    '/dedicate',
+    '/stack',
+    '/projects',
+  ];
+  currentRouter: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.router.events.subscribe((e) => {
+      this.currentRouter = location.pathname;
+    });
   }
 
+  rename() {
+    this.currentRouter = 'jhony';
+  }
+
+  ngOnInit(): void {}
 }
