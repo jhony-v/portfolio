f695cba86496fa7f22827b5c44ff53bfb7b93efa
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-01-28 11:12:55 -0500

    Remove api from repository

--- api/src/index.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-import server from "./server"
-
-const PORT = process.env.PORT || 7000;
-server.listen(PORT)
--- api/src/validators/sendEmailValidator.ts
+++
@@ -1,8 +0,0 @@
-import { body } from "express-validator";
-
-export const sendEmailValidator = [
-    body("from").exists().notEmpty().isEmail(),
-    body("subject").exists().notEmpty(),
-    body("message").exists().notEmpty(),
-    body("name").exists().notEmpty()
-]
