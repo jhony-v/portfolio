5fcb8c499901abc871b230918a3560386ddd2641
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-01-29 17:55:18 -0500

    feat: Add feature to move between projects with the keyboard

--- src/views/IndexView/sections/PrevisualizeProjects/containers/PreviewProject/ChevronsChangeProject.tsx
+++ src/views/IndexView/sections/PrevisualizeProjects/containers/PreviewProject/ChevronsChangeProject.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react"
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs"
-import useNavigateBetweenProjects from '../../hooks/useNavigateBetweenProjects';
-import ChevronMove from './components/ChevronMove';
+import startInClient from "utils/startInClient"
+import useNavigateBetweenProjects from "../../hooks/useNavigateBetweenProjects"
+import ChevronMove from "./components/ChevronMove"
 
 export default function ChevronsChangeProject() {
-    const { onNext, onPrevious } = useNavigateBetweenProjects();
-    return(
-      <div className="flex justify-between mt-5 md:mt-0">
-        <ChevronMove onClick={onPrevious}  position="left"><BsChevronLeft/></ChevronMove>
-        <ChevronMove onClick={onNext} position="right"><BsChevronRight/></ChevronMove>
-      </div>
-    )
+  const { onNext, onPrevious } = useNavigateBetweenProjects()
+
+  useEffect(() => {
+    startInClient(() => {
+      const handler = (event: KeyboardEvent) => {
+        switch (event.key) {
+          case "ArrowLeft":
+            onPrevious();
+            break
+          case "ArrowRight":
+            onNext();
+            break
+        }
+      }
+      window.addEventListener("keyup", handler)
+      return () => {
+        window.removeEventListener("keyup", handler)
+      }
+    })
+  }, [])
+
+  return (
+    <div className="flex justify-between mt-5 md:mt-0">
+      <ChevronMove onClick={onPrevious} position="left">
+        <BsChevronLeft />
+      </ChevronMove>
+      <ChevronMove onClick={onNext} position="right">
+        <BsChevronRight />
+      </ChevronMove>
+    </div>
+  )
 }
--- src/views/IndexView/sections/PrevisualizeProjects/containers/PreviewProject/components/CardTechnologyUsed.tsx
+++ src/views/IndexView/sections/PrevisualizeProjects/containers/PreviewProject/components/CardTechnologyUsed.tsx
@@ -3,7 +3,7 @@ import React from "react"
 type CardTechnologyUsedProps = { image; title }
 const CardTechnologyUsed = ({ image, title }: CardTechnologyUsedProps) => (
   <div className="bg-gray-600 rounded-3xl inline-flex items-center py-3 px-5 mr-4 mb-4">
-    <img className="w-6 h-7 md:w-8 md:h-8 object-cover" src={image} />
+    <img className="w-6 h-7 md:w-8 md:h-8 object-cover rounded-full" src={image} />
     <span className="font-bold ml-2 text-label">{title}</span>
   </div>
 )
